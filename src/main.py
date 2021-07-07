b95c19e150f1b6df672a90b650f1d74937b71d9c
Jaeyeon Kim <anencore94@gmail.com>
2021-07-07 21:55:33 +0900

    api test

--- src/main.py
+++ src/main.py
@@ -16,3 +16,19 @@ if __name__ == "__main__":
     print(open_id)
     print(merged_id)
     print(closed_id)
+
+    # when pr opened with "Fixed [ch-xxxx]" in pr body
+    # send request to move ch story
+
+    headers = {"Content-Type": "application/json", "Clubhouse-Token": clubhouse_api_token}
+
+    BASE_URL = "https://api.clubhouse.io/api/v3/stories/"
+    parsed_story_id = str(6027)
+
+    request_body = dict()
+    request_body["workflow_state_id"] = open_id
+
+    res = requests.put(url=BASE_URL + parsed_story_id, data=request_body, headers=headers)
+
+    print(res.status_code)
+    print(res.text)
