e585a9eb0ffe7690bc4751ca9d28294bc61187aa
Jaeyeon Kim <anencore94@gmail.com>
2021-07-07 22:22:47 +0900

    test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.9-slim-buster
 
+ENV TZ=Asia/Seoul
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
--- src/main.py
+++ src/main.py
@@ -3,40 +3,43 @@ import json
 import os
 import requests
 
+def parse_from_pr_body():
+
+    return str(6027)
+    pass
+
 if __name__ == "__main__":
-    print("hello world")
+    print(os.environ)
 
+    exit(1)
     clubhouse_api_token = os.environ["INPUT_CLUBHOUSE_API_TOKEN"]
-    print(clubhouse_api_token)
-
     open_id = os.environ["INPUT_PR_OPENED"]
     merged_id = os.environ["INPUT_PR_MERGED"]
     closed_id = os.environ["INPUT_PR_CLOSED"]
 
-    print(open_id)
-    print(type(open_id))
-    print(merged_id)
-    print(closed_id)
+    # TODO detailed validation check
+    if not (clubhouse_api_token and open_id and merged_id and closed_id):
+        print("some of your inputs are invalid")
+        exit(1)
 
     # when pr opened with "Fixed [ch-xxxx]" in pr body
     # send request to move ch story
-
     headers = {"Content-Type": "application/json", "Clubhouse-Token": clubhouse_api_token}
-
     BASE_URL = "https://api.clubhouse.io/api/v3/stories/"
-    parsed_story_id = str(6027)
 
+    parsed_story_id = parse_from_pr_body()
     request_body = {
-        "workflow_state_id" : str(open_id)
+        "workflow_state_id": str(open_id)
     }
 
+    print("sending clubhouse api")
     res = requests.put(url=BASE_URL + parsed_story_id, data=json.dumps(request_body), headers=headers)
-    print(res.status_code)
-    print(res.text)
-
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        # Whoops it wasn't a 200
+        # if not 200
+        print(res.status_code)
+        print(res.text)
         exit(1)
 
+    print("clubhouse story workflow updated successfully!")
