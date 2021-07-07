2bcb8d10232f3505399916ce8e8bcd360b8a3fa3
Jaeyeon Kim <anencore94@gmail.com>
2021-07-07 21:38:48 +0900

    bugfix

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import requests
 if __name__ == "__main__":
     print("hello world")
     clubhouse_api_token = os.environ["INPUT_CLUBHOUSE_API_TOKEN"]
-    config_path = os.environ["INPUT_CLUBHOUSE_CONFIG_PATH"]
+    config_path = os.environ["INPUT_CONFIG_PATH"]
     my_output = f"Hello {clubhouse_api_token}"
 
     print(my_output)
