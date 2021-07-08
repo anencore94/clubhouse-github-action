0575f69e3c2fcb3dde2655d97265674986d45620
Jaeyeon Kim <anencore94@gmail.com>
2021-07-08 11:21:57 +0900

    resolve conflicts

--- src/main.py
+++ src/main.py
@@ -1,54 +1,96 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import re
+
 import requests
 
-def parse_from_pr_body():
 
-    return str(6027)
-    pass
+def get_desired_workflow_state_id(state):
+    open_workflow_state = os.environ["INPUT_PR_OPENED"]
+    closed_id = os.environ["INPUT_PR_CLOSED"]
+
+    print(f"current pr status is {state}")
+    if state == "open":
+        return open_workflow_state
+
+    return closed_id
+
+
+def parse_from_pr_body(body):
+    ch_0_regexp = re.compile(r'\[ch-0\]')
+    if ch_0_regexp.search(body):
+        print("nothing to do with clubhouse story")
+        # pass if ch-0
+        exit(0)
+
+    regexp = re.compile(r'Fixes \[ch-\d+\]')
+    if regexp.search(body):
+        print('matched')
+        candidates = re.findall(regexp, body)
+        print(candidates)
+
+    return candidates
+
 
 if __name__ == "__main__":
-    print(os.environ["INPUT_PR_NUMBER"])
-
-    exit(1)
-    # TODO
-    # github pull number 알아낸다음
-    # curl   -H "Accept: application/vnd.github.v3+json"   https://api.github.com/repos/anencore94/clubhouse-github-action/pulls/{pull_request_number}
-    # api 날려서 (token 담아야 함)
-    # response.body['body'] 가 pr_body 의 컨텐츠이고,
-    # 거기에 Fixes [ch-xxxx] 가 있는지 확인한 다음, parsing 한다.
-
-
-
+    gh_token = os.environ["INPUT_GITHUB_TOKEN"]
+    pr_num = os.environ["INPUT_PR_NUMBER"]  # 1
+    repo_name = os.environ["GITHUB_REPOSITORY"]  # anencore94/clubhouse-github-action
     clubhouse_api_token = os.environ["INPUT_CLUBHOUSE_API_TOKEN"]
     open_id = os.environ["INPUT_PR_OPENED"]
-    merged_id = os.environ["INPUT_PR_MERGED"]
     closed_id = os.environ["INPUT_PR_CLOSED"]
 
     # TODO detailed validation check
-    if not (clubhouse_api_token and open_id and merged_id and closed_id):
+    if not (gh_token and pr_num and repo_name and clubhouse_api_token and open_id and closed_id):
         print("some of your inputs are invalid")
         exit(1)
 
-    # when pr opened with "Fixed [ch-xxxx]" in pr body
-    # send request to move ch story
-    headers = {"Content-Type": "application/json", "Clubhouse-Token": clubhouse_api_token}
-    BASE_URL = "https://api.clubhouse.io/api/v3/stories/"
+    # Fetch current pull request body
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"token {gh_token}"}
+    URL = f"https://api.github.com/repos/{repo_name}/pulls/{pr_num}"
 
-    parsed_story_id = parse_from_pr_body()
-    request_body = {
-        "workflow_state_id": str(open_id)
-    }
-
-    print("sending clubhouse api")
-    res = requests.put(url=BASE_URL + parsed_story_id, data=json.dumps(request_body), headers=headers)
+    print("sending github api")
+    res = requests.get(url=URL, headers=headers)
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError as e:
         # if not 200
         print(res.status_code)
-        print(res.text)
+        print(res.json())
+        print("github token or github relevant inputs are invalid")
         exit(1)
+
+    pr_body = res.json()["body"]
+    print(f"pr_body is {pr_body}")
+
+    # parse story id from pr_body
+    stories = []
+    for candidate in parse_from_pr_body(body=pr_body):
+        stories.append(candidate[10:-1])  # if candidate : Fixes [ch-xxxx], story : xxxx
+    print(stories)
+
+    # check current pr's state and find desired_workflow_state_id
+    desired_workflow_state_id = get_desired_workflow_state_id(state=res.json()["state"])
+
+    # send request to move storys' workflow state
+    headers = {"Content-Type": "application/json", "Clubhouse-Token": clubhouse_api_token}
+    BASE_URL = "https://api.clubhouse.io/api/v3/stories/"
+
+    request_body = {
+        "workflow_state_id": str(desired_workflow_state_id)
+    }
+
+    print("sending clubhouse api")
+    for story in stories:
+        res = requests.put(url=BASE_URL + story, data=json.dumps(request_body), headers=headers)
+        try:
+            res.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            # if not 200
+            # TODO error msg 가 not found 이면 pass
+            print(res.status_code)
+            print(res.text)
+            exit(1)
 
     print("clubhouse story workflow updated successfully!")
